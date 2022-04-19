b98a97efa0e77d0d7078912850ce2fbd7cf3aa96
gabriel-alvesV <98417878+gabriel-alvesV@users.noreply.github.com>
2022-04-19 17:46:17 -0300

    piratas

--- classes/bala.js
+++ classes/bala.js
@@ -6,6 +6,7 @@ class Bala {
         }
         this.corpo = Bodies.circle(x, y, this.raio, options);
         this.imagem = loadImage("./assets/cannonball.png");
+        this.mostrarrastro=[];
         World.add(world, this.corpo);
     }
 
@@ -15,14 +16,24 @@ class Bala {
         imageMode(CENTER);
         image(this.imagem, pos.x, pos.y, this.raio, this.raio);
         pop();
+        if(this.corpo.velocity.x>0&&pos.x>10){
+            var rastro=[pos.x,pos.y];
+            this.mostrarrastro.push(rastro);
+        }
+        for(var i=0;i<this.mostrarrastro.length;i++){
+            image(this.imagem,this.mostrarrastro[i][0],this.mostrarrastro[i][1],5,5);
+        }
     }
 
     atirar(){
+        var novoAng = canhao.ang - 28;
+        novoAng = novoAng * (3.14/180);
+        var velocidade = p5.Vector.fromAngle(novoAng);
+        velocidade.mult(0.5);
         Matter.Body.setStatic(this.corpo,false);
         Matter.Body.setVelocity(this.corpo,{
-                        x:30,
-                        y:-20,
-        
+                        x:velocidade.x * (180/3.14),
+                        y:velocidade.y * (180/3.14),
         })
     }
 }
--- sketch.js
+++ sketch.js
@@ -10,6 +10,9 @@ var torreImagem;
 var canhao;
 var angulo;
 var bala;
+var matrizbala=[];
+
+
 
 function preload() {
   fundo = loadImage("./assets/background.gif");
@@ -35,7 +38,7 @@ function setup() {
  angulo=20;
  canhao=new Canhaopdf(180,110,130,100,angulo);
 
- bala = new Bala(canhao.posX, canhao.posY);
+ 
  
 }
 
@@ -52,11 +55,52 @@ function draw() {
  image(torreImagem,torre.position.x, torre.position.y, 160, 310);
  pop();
  canhao.mostrar();
- bala.mostrar();
+ for(var i=0;i<matrizbala.length;i++){
+   mostrarbala(matrizbala[i],i);
+ }
   
    
 }function keyReleased(){
   if(keyCode===DOWN_ARROW){
-    bala.atirar();
+    matrizbala[matrizbala.length-1].atirar();
   }
 }
+
+function keyPressed(){
+  if(keyCode===DOWN_ARROW){
+   var bala = new Bala(canhao.posX, canhao.posY);
+   matrizbala.push(bala);
+  }
+}
+
+function mostrarbala(bala,i){
+  if(bala){
+    bala.mostrar();
+  }
+}
+
+
+//Revisão de Matrizes
+
+//Matriz apenas com números
+var matriz1 = [6,7,8,1,2];
+//console.log(matriz1);
+
+//Matriz com vários tipos de dados
+var matriz2 = [59, "Gabriel", true, "Batata"];
+//console.log(matriz2);
+
+//Matriz de matrizes
+var matriz3 = [matriz1, matriz2];
+//console.log(matriz3);
+
+//Acessar elementos das matrizes de acordo com o índice
+//console.log(matriz1[3]);
+//console.log(matriz2[2]);
+//console.log(matriz3[1][1]);
+
+//Adicionar e retirar elementos das matrizes
+matriz1.push(35);
+//console.log(matriz1);
+matriz1.pop();
+//console.log(matriz1);
